7af040eea0bdea032622d211fd1e87434331a785
innademidova <doc.idemidova@gmail.com>
2025-04-02 11:23:50 +0200

    Fixed double Back to Login buttons

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 export const env = {
   isDevelopment: import.meta.env.DEV,
   isProduction: import.meta.env.PROD,
-  baseUrl: import.meta.env.VITE_BASE_URL,
+  baseUrl: import.meta.env.VITE_BASE_URL || (import.meta.env.PROD ? window.location.origin : 'http://localhost:8080'),
   getResetPasswordUrl: () => `${env.baseUrl}/reset-password`,
 }; 
